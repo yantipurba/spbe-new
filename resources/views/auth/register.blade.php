9f30977542924d6bd43dcbae71deb25c99404859
karynleoni <107824186+karynleoni@users.noreply.github.com>
2023-07-21 16:34:35 +0700

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
+<link rel="stylesheet" href="{{ asset('assets/css/login.css') }}">
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
+
+                 <div class="login-container">
+                    <div class="logo">
+                        <img src="assets/images/logo.jpg" alt="Logo"></div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
